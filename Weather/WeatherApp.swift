41c5bc1e32999dd53661def64facc80aade69db2
Pinheiro-dev <mathepba@gmail.com>
2024-03-21 23:08:58 -0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Matheus Pinheiro on 21/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
